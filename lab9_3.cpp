3fbd334053470437bdd79339b334c3bb71670900
Jongkonrat <jongkonrat_pisit@cmu.ac.th>
2021-01-08 17:37:23 +0700

    completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,28 @@
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
+
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+#include<cstdlib>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int count=0;
+    float sum=0, sumSq=0;
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source,textline)){
+        sum += atof(textline.c_str());
+        sumSq += pow(atof(textline.c_str()),2);
+        count++;
+}
+
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << sum/count << endl;
+    cout << "Standard deviation = " << pow((float)1/count*sumSq-(float)pow((sum/count),2),0.5);
+    return 0;
+}
